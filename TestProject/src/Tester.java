2f7cd30206367d2405ea67fd993c9f7113ec812e
LENOVO <LENOVO@DESKTOP-OJ6ME36>
2023-07-09 19:23:54 +0530

    Second Commit

---
+++ TestProject/src/Tester.java
@@ -0,0 +1,14 @@
+
+public class Tester {
+	int num1=5;
+	int num2=10;
+	
+	public void add(){
+		System.out.println("Sum is" +(num1+num2));
+	}
+    public static void main(String arg[])
+    {
+    	Tester Test =new Tester();
+    	Test.add();
+    }
+}
